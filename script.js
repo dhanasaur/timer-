c0622ffaa0af78ac4142ac8c9f4f0d2f1089dd25
Dhana Sundar <dsdhana03@gmail.com>
2025-12-08 16:35:41 +0530

    jkjjjj

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function startTimer() {
   if (isNaN(sec) || sec < 0 || sec > 59) sec = 0;
   let totalSeconds = min * 60 + sec;
   if (totalSeconds <= 0) {
-    alert("enter a valid time");
     return;
   }
   clearInterval(timer);
@@ -30,3 +29,4 @@ function updateDisplay(seconds) {
   countdownElement.textContent = 
     `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
 }
+
